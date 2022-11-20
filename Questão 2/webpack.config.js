e0e895c05cd50b4226748dde2f0bc1bda0cf3f04
Felipe Saraiva <48689789+saraivas@users.noreply.github.com>
2022-11-20 08:18:45 -0300

    Add files via upload

---
+++ Questão 2/webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["vue-style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  // plugin omitted
+};
